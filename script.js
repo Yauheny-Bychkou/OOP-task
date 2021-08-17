299610b098e0e832b09329314ac5253652cad79e
Yauheny Bychkou <375445433672@yandex.by>
2021-08-17 09:44:41 +0300

    ready101percent

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 "use strict";
-let one = document.querySelector(".one");
 
-function DomElement(selector, height, width, bg, fontSize) {
+function DomElement(selector, height, width, bg, fontSize, position) {
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
+  this.position = position;
 }
 
-let element = new DomElement(".block'", "100px", "100px", "red", "25px");
+let element = new DomElement(".block'", "100px", "100px", "red", "25px", "absolute");
 
 DomElement.prototype.method = function () {
-  if (this.selector.charAt(0) === ".") {
+  const _this = this;
+
+  let ready = function () {
     let newDiv = document.createElement("div");
     newDiv.className = "block";
     newDiv.innerHTML = "Привет мир";
-    newDiv.style.cssText = ` height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
+    newDiv.style.cssText = ` height: ${_this.height}; width: ${_this.width}; background: ${_this.bg}; font-size: ${_this.fontSize}; position: ${_this.position};`;
     document.body.appendChild(newDiv);
+    let eventFunc = function (e) {
+      console.log(e.code);
+      if (e.code === "ArrowDown") {
+        newDiv.style.marginTop = "10px";
+      } else if (e.code === "ArrowUp") {
+        newDiv.style.marginTop = "-10px";
+      }
+      if (e.code === "ArrowLeft") {
+        newDiv.style.marginLeft = "-10px";
+      } else if (e.code === "ArrowRight") {
+        newDiv.style.marginLeft = "10px";
+      }
+    };
+    document.addEventListener("keydown", eventFunc);
+  };
 
-    //one.innerHTML = '<div class=block">class</div>';
-  } else if (this.selector.charAt(0) === "#") {
-    let newDiv = document.createElement("div");
-    newDiv.id = "best";
-    newDiv.innerHTML = "Привет друзья";
-    newDiv.style.cssText = ` height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize};`;
-    document.body.appendChild(newDiv);
-  }
+  document.addEventListener("DOMContentLoaded", ready);
 };
 element.method();
